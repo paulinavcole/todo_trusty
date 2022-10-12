1f916e3e198ce3e8a3401b75597ff5c27525221b
Paulina Cole <lina.cole@gmail.com>
2022-10-12 17:27:08 -0400

    working app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,164 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect, useRef } from 'react';
+import Data from './data.json'
+import { v1 as uuidv1} from 'uuid';
+import axios from 'axios';
+
 
 function App() {
+
+  // Reference
+  const titleRef = useRef();
+  const contentRef = useRef();
+
+  // State
+  const [data, setData] = useState(Data);
+
+  // Temp State
+  const [title, setTitle] = useState();
+  const [content, setContent] = useState();
+
+  const [updateID, setUpdateID] = useState();
+  const [updateTitle, setUpdateTitle] = useState();
+  const [updateContent, setUpdateContent] = useState();
+
+  // Effect
+  useEffect(() => {
+    //console.log(data);
+    // setData(Data);
+  }, [data]);
+
+  // Add Task
+  const addTask = () => {
+    if(title && content) {
+      // create new task object
+      let newTask = {
+        id: uuidv1(), 
+        "title": title, 
+        "content": content
+      }
+      // merge new tasks with copy of old state
+      let tasks = [...data, newTask];
+      // push new object to state
+      setData(tasks);
+      // clear title and content from state
+      setTitle();
+      setContent();
+
+      saveJSON(tasks);
+    }
+  }
+
+  // Delete Task
+  const deleteTask = (key) => {
+    // filter out task containing that id(key)
+    let filteredTasks = [...data].filter(task=> task.id !== key);
+    // save the rest in state
+    setData(filteredTasks);
+
+    // write to json file
+    saveJSON(filteredTasks);
+  }
+
+  // Update Task
+  const updateTask = () => {
+    // populate task info from temp state and prepare new obj for updated task
+    let editedTask = {
+      "id": updateID,
+      'title': updateTitle,
+      'content': updateContent
+    }
+    // remove old task with same ID and get remaining data
+    let filteredTasks = [...data].filter(task=> task.id !== updateID);
+    let tasks = [...filteredTasks, editedTask];
+    // push into state
+    setData(tasks)
+
+    // clear fields
+    setUpdateID()
+    setUpdateTitle()
+    setUpdateContent()
+
+    // write to json file
+    saveJSON(tasks);
+  }
+
+  // Populate Task
+  const populateTask = (key, title, content) => {
+    setUpdateID(key);
+    setUpdateTitle(title);
+    setUpdateContent(content);
+
+  }
+
+  // Write to JSON file
+
+  // this function will receive all updated state/tasks after you add/delete/edit tasks
+  const saveJSON = (tasks) => {
+    // api URL//end point from express server
+    const url = 'http://localhost:4999/write';
+    
+    axios.post(url, tasks)
+    .then(response => {
+      //console.log(response);
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className='tasks'>
+        <h1>To Do List</h1>
+
+      <div>
+        <h4>Add New Task</h4>
+        <input placeholder='Task Title' 
+          onChange={ e => setTitle( e.target.value )}
+          value= { title || ''}
+          ref={ titleRef }
+          />
+        <br />
+        <textarea placeholder='Task Content'
+          onChange={ e => setContent( e.target.value )}
+          value= { content || ''}
+          ref={ contentRef }
+        ></textarea>
+        <br />
+        <button onClick={ addTask }>Add</button>
+      </div>
+
+      {/* If temp state has values of title or content for update form, show this:  */}
+
+      {updateTitle || updateContent ? (
+        <div>
+           <h4>Update Task</h4>
+           <input placeholder='Task Title'
+             onChange={ e => setUpdateTitle( e.target.value )}
+             value= { updateTitle || ''}
+           />
+           <br />
+           <textarea placeholder='Task Content'
+             onChange={ e => setUpdateContent( e.target.value )}
+             value= { updateContent || ''}
+           ></textarea>
+           <br />
+           <button onClick={ updateTask }>Update</button>
+        </div>
+      ) : null }
+  
+
+
+        { data? data.map(task => {
+          return(
+            <div key={ task.id }>
+              <h4>{ task.title }</h4>
+              <p>{ task.content }</p>
+              <button onClick={() => populateTask(task.id, task.title, task.content)}>Edit Task</button>
+              <button onClick={() => deleteTask(task.id)}>Delete Task</button>
+            </div>
+          ) 
+        }) : null}
+      </div>
     </div>
   );
 }
---
+++ src/server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const fs = require('fs');
+const morgan = require('morgan');
+const cors = require('cors');
+
+// declare app
+const app = express();
+const port = 4999;
+
+// middleware
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use(cors());
+
+// default route
+app.get('/', (req, res) => res.status(200).send({
+    message: 'Server is running!'
+}));
+
+const WriteTextToFile = async (content) => {
+    fs.writeFile('./src/data.json', content, (err) => {
+        console.log(content);
+        if (err) {
+            console.log(err)
+        } else {
+            console.log('written to file')
+        }
+    })
+}
+
+// post route
+app.post('/write', async(req, res, next) => {
+    const requestContent = JSON.stringify(req.body);
+    await WriteTextToFile(requestContent)
+})
+
+// 404 route
+app.use((req, res, next) => res.status(404).send({
+    message: 'Page not found!'
+}));
+
+// run server
+app.listen(port, () => {
+    console.log(`
+    listening on port ${port}
+    http://localhost:4999
+    `
+    )
+})
